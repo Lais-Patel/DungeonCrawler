c7faf35cb558c6c20c5a50de866ddc17d7de324d
Mango <plais999@gmail.com>
2023-07-30 22:36:18 +0100

    Attempted to add Dash
    Added necessary variables for dashing. Put movement calculation in a method.
    When dashing the entity becomes unable to interact with movement, however the entity gets stuck in place.

--- Assets/Entity/Scripts/EntityMovement.cs
+++ Assets/Entity/Scripts/EntityMovement.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class EntityMovement : MonoBehaviour
 {
-    public float maxSpeed = 10f;
-    public float acceleration = 0.6f;
+    public float maxSpeed = 6f;
+    public float velocity;
+    public float acceleration = 0.1f;
     public Rigidbody2D rb;
+
+    public float dashPower = 10f;
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    private bool hasPressedDash;
+    private bool canDash = true;
 
     private Vector2 directionMovement;
     private Vector2 directionMovementSmooth;
@@ -17,6 +24,11 @@ public class EntityMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (hasPressedDash)
+        {
+            return;
+        }
+
         directionMovement.x = Input.GetAxisRaw("Horizontal");
         directionMovement.y = Input.GetAxisRaw("Vertical");
 
@@ -25,14 +37,45 @@ public class EntityMovement : MonoBehaviour
         animationController.SetFloat("Velocity", directionMovement.sqrMagnitude);
 
         directionMovement = directionMovement.normalized;
+
+        if (Input.GetButtonDown("Dash") && canDash)
+        {
+            StartCoroutine(dashAlgorithm());
+        }
         
     }
 
     // Update is called at fixed increments
     void FixedUpdate()
     {
-        directionMovementSmooth = Vector2.SmoothDamp(directionMovementSmooth, directionMovement, ref directionMovementSmoothRef, acceleration);
+        if (hasPressedDash)
+        {
+            return;
+        }
+        else if (!hasPressedDash)
+        {
+            velocity = maxSpeed;
+        }
 
-        rb.MovePosition(rb.position + directionMovementSmooth * Time.fixedDeltaTime * maxSpeed);
+        EntityMovementCalc();
+    }
+
+    private void EntityMovementCalc()
+    {
+        directionMovementSmooth = Vector2.SmoothDamp(directionMovementSmooth, directionMovement, ref directionMovementSmoothRef, acceleration);
+        rb.MovePosition(rb.position + directionMovementSmooth * Time.fixedDeltaTime * velocity);
+    }
+
+    private IEnumerator dashAlgorithm()
+    {
+        hasPressedDash = true;
+        canDash = false;
+        velocity = dashPower;
+        directionMovementSmooth = Vector2.SmoothDamp(directionMovementSmooth, directionMovement, ref directionMovementSmoothRef, acceleration);
+        rb.MovePosition(rb.position + directionMovementSmooth * Time.fixedDeltaTime * velocity);
+        yield return new WaitForSeconds(dashLength);
+        hasPressedDash = false;
+        yield return new  WaitForSeconds(dashCooldown);
+        canDash = true;
     }
 }
