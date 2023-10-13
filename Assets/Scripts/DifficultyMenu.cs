f979a48f8589811e27cc1e72a74872e3fb805e19
Mango <plais999@gmail.com>
2023-10-13 20:39:45 +0100

    Difficulty Desriptions
    Correctly changes, upon changing the value, between the placeholder strings.

--- Assets/Scripts/DifficultyMenu.cs
+++ Assets/Scripts/DifficultyMenu.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DifficultyMenu : MonoBehaviour
 {
     public Slider difficultyBar;
-	public Text difficultyDescription;
+	public TextMeshProUGUI difficultyDescription;
     public static float difficultyRating;
     
     
@@ -27,19 +28,19 @@ public class DifficultyMenu : MonoBehaviour
 	
 		if (difficultyRating == 1)
 		{
-			difficultyDescription.Text = "Placeholder 1";
+			difficultyDescription.text = "Placeholder 1";
 		}
 		else if (difficultyRating == 2)
 		{
-			difficultyDescription.Text = "Placeholder 2";
+			difficultyDescription.text = "Placeholder 2";
 		}
 		else if (difficultyRating == 3)
 		{
-			difficultyDescription.Text = "Placeholder 3";
+			difficultyDescription.text = "Placeholder 3";
 		}
 		else if (difficultyRating == 4)
 		{
-			difficultyDescription.Text = "Placeholder 4";
+			difficultyDescription.text = "Placeholder 4";
 		}
     }
 }
