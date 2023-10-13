e9178592d07a71338f443c906147c7b3c2d4e846
Mango <plais999@gmail.com>
2023-10-13 20:46:47 +0100

    Difficulty Description
    Updated the difficulty description to show what each difficulty will do to the game.

--- Assets/Scripts/DifficultyMenu.cs
+++ Assets/Scripts/DifficultyMenu.cs
@@ -28,19 +28,27 @@ public class DifficultyMenu : MonoBehaviour
 	
 		if (difficultyRating == 1)
 		{
-			difficultyDescription.text = "Placeholder 1";
+			difficultyDescription.text = "Player Health 200%     " +
+			                             "Player Damage 200%     " +
+			                             "Enemy Spawns 0.5x      ";
 		}
 		else if (difficultyRating == 2)
 		{
-			difficultyDescription.text = "Placeholder 2";
+			difficultyDescription.text = "Player Health 100%     " +
+			                             "Player Damage 100%     " +
+			                             "Enemy Spawns 1x        ";
 		}
 		else if (difficultyRating == 3)
 		{
-			difficultyDescription.text = "Placeholder 3";
+			difficultyDescription.text = "Player Health 75%      " +
+			                             "Player Damage 75%      " +
+			                             "Enemy Spawns 1.5x      ";
 		}
 		else if (difficultyRating == 4)
 		{
-			difficultyDescription.text = "Placeholder 4";
+			difficultyDescription.text = "Player Health 3        " +
+			                             "Player Damage 50%      " +
+			                             "Enemy Spawns 3x        ";
 		}
     }
 }
